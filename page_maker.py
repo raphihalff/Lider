2927cd1519c84ad89e96364f3206bccb85c837ab
raphi <rhalff@yiddishbookcenter.org>
2017-12-22 14:03:51 -0500

    License created

--- page_maker.py
+++ page_maker.py
@@ -1,5 +1,21 @@
 # This Python file uses the following encoding: utf-8
 # This reads all poem data and formats it into html page format
+# Copyright (C) 2017-present,  Raphael Halff
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# This source code is licensed under the license found in the 
+# LICENSE file in the root directory of this source tree, 
+# but you can also find it here: <http://www.gnu.org/licenses/>.
+
 import os
 import fnmatch
 import re
