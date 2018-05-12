73afca7b3982dec977bf0354ad80692624f1ab61
Raphael Halff <raphi.halff@gmail.com>
2018-05-12 16:55:23 -0400

    added link to about page

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 			if ($mysql->connect_error) {
 				die("Connection failed: " . $mysql->connect_error);
 			}
-		?> 
+		?>
         <div class="browse_btns">
             <button class="browse_btn yid" id="yid_poet_btn" dir="rtl">די דיכטערס</button>
             <button class="browse_btn yid cur_browse_btn" id="yid_poem_btn" dir="rtl">די לידער</button>
@@ -35,7 +35,7 @@
         <div class="frame">
             <h2 class="browse_hdr eng" id="poet_hdr_eng">Browse the Poets</h2>
             <h2 class="browse_hdr yid default" id="poet_hdr_yid" dir="rtl">בלעטערעט איבער די דיכטערס</h2>
-           
+
             <h2 class="browse_hdr eng" id="poem_hdr_eng">Browse the Poems</h2>
             <h2 class="browse_hdr yid" id="poem_hdr_yid" dir="rtl">בלעטערעט איבער די לידער</h2>
 
@@ -81,7 +81,7 @@
             		}
             	?>
             </ul>
-            
+
             <ul class="link_list eng" id="poet_list_eng">
             	<?php
             		$sql = "SELECT name_y, name_e,img FROM poet ORDER BY name_e;";
@@ -95,7 +95,7 @@
             		}
             	?>
             </ul>
-            
+
             <ul class="link_list" id="year_list">
             	<?php
             		$sql = "SELECT DISTINCT YEAR(date) FROM poem ORDER BY date;";
@@ -103,7 +103,7 @@
             		if ($results->num_rows > 0) {
             			while($result = $results->fetch_assoc()) {
             				$sql = "SELECT * FROM poem WHERE YEAR(date)=" . $result['YEAR(date)'];
-							$how_many_poems = $mysql->query($sql)->num_rows;    				
+							$how_many_poems = $mysql->query($sql)->num_rows;
             				echo '<li class="link_list_item"><div class="link_box"><form action="year.php" method="get"><button type="submit" class="poem_link" name="year" value="' . $result['YEAR(date)'] . '"><h3 class="link_title">' . $result['YEAR(date)'] . ' (' . $how_many_poems . ')' . '</h3></button></form></div></li>';
             			}
             		}
@@ -111,12 +111,9 @@
             </ul>
         </div>
         </div>
-        
-        
+        <a class="about" href="about.html">About Us / װעגן אונדז</a>
+        <script src="browse.js"></script>
 
-        <script src="browse.js"></script>  
-        
-   	
 	<div id="license" style="float: right; font-size: xx-small; width: 150px; text-align: justify; margin:auto; padding: 10px; display: block;">
 		<a rel="license" ref="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="display: block; border-width:0; margin: 0 auto;" src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png" /></a><br />Except where otherwise noted, content on this site is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons Attribution-ShareAlike 4.0 International License</a>.
 	</div>
