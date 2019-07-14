7566f24fe485fe98f990e6443b7c0d9dfa52402a
Raphael Halff <raphi.halff@gmail.com>
2019-07-14 19:55:37 +0300

    and more

--- exhibits/togyiddish/header.php
+++ exhibits/togyiddish/header.php
@@ -9,10 +9,10 @@
     	<h1 class="homepage yid" dir="rtl">
 
         <div class="hdr1" style="grid-column: 5; grid-row: <?php echo rand(1,4); ?>; color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">ד</div>
-        <div class="hdr2">אָ</div>
-        <div class="hdr3">ר</div>
-        <div class="hdr4">ט</div>
-        <div class="hdr5">ן</div>
+        <div class="hdr2" style="grid-column: 4; grid-row: <?php echo rand(1,4); ?>; color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">אָ</div>
+        <div class="hdr3" style="grid-column: 3; grid-row: <?php echo rand(1,4); ?>; color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">ר</div>
+        <div class="hdr4" style="grid-column: 2; grid-row: <?php echo rand(1,4); ?>; color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">ט</div>
+        <div class="hdr5" style="grid-column: 1; grid-row: <?php echo rand(1,4); ?>; color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">ן</div>
       </h1>
     </a>
   </div>
