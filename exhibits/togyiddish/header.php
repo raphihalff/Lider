08a1e3a73a661a05937e7d7da7d2a055b5368e05
Raphael Halff <raphi.halff@gmail.com>
2019-07-14 20:33:48 +0300

    more

--- exhibits/togyiddish/header.php
+++ exhibits/togyiddish/header.php
@@ -2,16 +2,16 @@
   $size = array("2.4em","2.2em","2em","1.8em","1.6em");
   $color = array("--main-gray", "--main-green", "--main-yellow", "--main-red", "--main-turq", "--main-turq", "--main-orange");
   $font = array("tamy", "frank", "vilna", "simple", "hadasim");
-  $title_yid = "איבערזעצונגען פֿון אַ מאָטיװ";
-  $title_eng = "Translations of a Theme"
+  $title_yid = array("א","י","ב","ע","ר","ז","ע","צ","ו","נ","ג","ע","ן"," ","פֿ","ו","ן"," ","אַ"," ","מ","אָ","ט","י","װ");
+  $title_eng = array("T","r","a","n","s","l","a","t","i","o","n","s"," ","o","f"," ","a"," ","T","h","e","m","e");
 ?>
 <header>
   <div class="hdr-wrapper">
     <a class="homepage" href="/">
     	<h1 class="homepage yid" dir="rtl">
         <?php
-        $chars = str_split($title_yid);
-        foreach ($chars as $letter) {
+
+        foreach ($title_yid as $letter) {
           echo '<div class="hdr-letter" style="color: var(' . $color[rand(0,count($color)-1)] . '); font-family: ' . $font[rand(0,count($font)-1)] . '; font-size: ' . $size[rand(0,count($size)-1)] . ';">' . $letter . '</div>';
         }
         ?>
