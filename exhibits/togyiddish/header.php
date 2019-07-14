3e59dc97208369e535c7de671bda8bce52995c1a
Raphael Halff <raphi.halff@gmail.com>
2019-07-14 20:25:41 +0300

    new header

--- exhibits/togyiddish/header.php
+++ exhibits/togyiddish/header.php
@@ -2,16 +2,18 @@
   $size = array("2.4em","2.2em","2em","1.8em","1.6em");
   $color = array("--main-gray", "--main-green", "--main-yellow", "--main-red", "--main-turq", "--main-turq", "--main-orange");
   $font = array("tamy", "frank", "vilna", "simple", "hadasim");
+  $title_yid = "איבערזעצונגען פֿון אַ מאָטיװ";
+  $title_eng = "Translations of a Theme"
 ?>
 <header>
   <div class="hdr-wrapper">
     <a class="homepage" href="/">
     	<h1 class="homepage yid" dir="rtl">
-        <div class="hdr1 hdr-letter" style="color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">ד</div>
-        <div class="hdr2 hdr-letter" style="color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">אָ</div>
-        <div class="hdr3 hdr-letter" style="color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">ר</div>
-        <div class="hdr4 hdr-letter" style="color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">ט</div>
-        <div class="hdr5 hdr-letter" style="color: <?php echo 'var(' . $color[rand(0,count($color)-1)] . ')'; ?>; font-family: <?php echo $font[rand(0,count($font)-1)]; ?>; font-size: <?php echo $size[rand(0,count($size)-1)]; ?>;">ן</div>
+        <?php
+        foreach ($title_yid as $letter) {
+          echo '<div class="hdr-letter" style="color: var(' . $color[rand(0,count($color)-1)] . '); font-family: ' . $font[rand(0,count($font)-1)] . '; font-size: ' . $size[rand(0,count($size)-1)] . ';">' . $letter . '</div>';
+        }
+        ?>
       </h1>
     </a>
   </div>
