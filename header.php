5bd489a55f7faab9d5287be77936fab90d7ede2c
Raphi Halff <raphi.halff@gmail.com>
2021-05-04 12:47:28 -0400

    correction to yiddish mistakes from ages ago

--- header.php
+++ header.php
@@ -7,3 +7,4 @@
     <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Foytser&width=70&layout=button&action=like&size=large&colorscheme=dark&show_faces=false&share=false&height=65&appId" width="70" height="65" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 </header>
 <script data-name="BMC-Widget" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="raphi" data-description="שטיצט מיר אונטער מיט אַ הערינג!" data-message="סײַ אַ קאַװע סײַ אַ פֿיש, װאָס איר קענט מיר געבן איז אַ הילף" data-color="#79D6B5" data-position="right" data-x_margin="10" data-y_margin="10"></script>
+
